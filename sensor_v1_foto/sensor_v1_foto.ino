153005d044fda41f97a9b4d5588010c08c0976e7
Otavio Cesario <otavio.jar@gmail.com>
2026-07-18 12:01:19 -0300

    feat: integra nova medicao de corrente

--- sensor_v1_foto/sensor_v1_foto.ino
+++ sensor_v1_foto/sensor_v1_foto.ino
@@ -1,35 +1,24 @@
-// #include <DHT.h>
-// #define DHT11_PIN  4 // ESP32 pin GPIO21 connected to DHT11 sensor
-// DHT dht11(DHT11_PIN, DHT11);
-#include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define SCREEN_ADDRESS 0x3C
+
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-#define NUMFLAKES     10 // Number of snowflakes in the animation example
-#define LOGO_HEIGHT   16
-#define LOGO_WIDTH    16
-static const unsigned char PROGMEM logo_bmp[] ={ 0b00000000, 0b11000000,  0b00000001, 0b11000000,  0b00000001, 0b11000000,  0b00000011, 0b11100000,  0b11110011, 0b11100000,  0b11111110, 0b11111000,  0b01111110, 0b11111111, 0b00110011, 0b10011111,  0b00011111, 0b11111100,  0b00001101, 0b01110000,  0b00011011, 0b10100000,  0b00111111, 0b11100000,  0b00111111, 0b11110000,  0b01111100, 0b11110000,  0b01110000, 0b01110000,  0b00000000, 0b00110000 };
 
 const int pinoADC = 34;
 const unsigned long taxaAmostragemHz = 2000UL;
 const unsigned long periodoAmostragemUs = 1000000UL / taxaAmostragemHz;
 // 200 ms correspondem a 12 ciclos completos de uma rede de 60 Hz.
-const int numeroAmostrasRms = 400;
-const int sctval = 10;
+const int numeroAmostras = 400;
 const int tensaoResidencia = 120;
-// Ajuste linear obtido a partir dos dados simulados do novo condicionador.
-// A tensao usada aqui e o RMS da componente AC, depois da remocao do offset.
-const float aFx = 35.5249;
-const float bFx = 0.0848;
-int ct = 0;
-// Caso use ADC_11db entao 3.3
-// Caso use ADC_0db entao 0.9
-const float atenuacao = 3.3;
+const float coeficienteA = 20.453f;
+const float coeficienteB = -0.7362f;
+const float raizDeDois = 1.41421356f;
+
 #define RELE1 26
 #define BUZZER 27
 
@@ -50,35 +39,45 @@ unsigned long inicioPico = 0;
 unsigned long inicioCondicaoLigar = 0;
 unsigned long momentoDesligamentoRele = 0;
 
-
 void setup() {
   pinMode(RELE1, OUTPUT);
   pinMode(BUZZER, OUTPUT);
-  // Desliga os relés ao iniciar
+  // Mantem o rele desligado durante a inicializacao.
   digitalWrite(RELE1, HIGH);
   digitalWrite(BUZZER, LOW);
 
   Serial.begin(115200);
-  analogReadResolution(12); // 0-4095
-  analogSetAttenuation(ADC_11db); // permite medir até ~3.3V
-  // analogSetAttenuation(ADC_6db); // permite medir até ~1.9V
-  Serial.begin(9600);
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for(;;); // Don't proceed, loop forever
+  analogReadResolution(12);
+  analogSetAttenuation(ADC_11db);
+
+  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("Falha ao inicializar o display SSD1306."));
+    for (;;) {
+      delay(1000);
+    }
   }
-  display.display();
-  delay(2000); // Pause for 2 seconds
+
   display.clearDisplay();
-  display.drawPixel(10, 10, SSD1306_WHITE);
   display.display();
 }
 
 void loop() {
-  float tensao = medirTensaoRms();
-  float corrente = tensao * aFx + bFx;
+  float vmin;
+  float vmax;
+
+  medirExtremosTensao(vmin, vmax);
+
+  float vpp = vmax - vmin;
+  float vp = vpp / 2.0f;
+  float vrms = vp / raizDeDois;
+  float corrente = coeficienteA * vrms + coeficienteB;
+
+  if (corrente < 0.0f) {
+    corrente = 0.0f;
+  }
+
   float potencia = corrente * tensaoResidencia;
-  displayLED(tensao, corrente, potencia);
+  displayLED(vpp, vrms, vmin, vmax, corrente, potencia);
 
   controlarRele(potencia);
   Serial.println("Enviando dados pela serial...");
@@ -86,31 +85,31 @@ void loop() {
   delay(500);
 }
 
-float medirTensaoRms() {
-  double mediaMv = 0.0;
-  double somaDesviosQuadrados = 0.0;
+void medirExtremosTensao(float &vmin, float &vmax) {
+  uint32_t minimoMv = 0;
+  uint32_t maximoMv = 0;
   unsigned long proximaAmostra = micros();
 
-  for (int i = 0; i < numeroAmostrasRms; i++) {
-    // Agenda cada conversao a partir do instante inicial. Assim, o tempo gasto
-    // em analogReadMilliVolts() nao e somado ao periodo de 500 us.
+  for (int i = 0; i < numeroAmostras; i++) {
     long tempoRestante = (long)(proximaAmostra - micros());
     if (tempoRestante > 0) {
       delayMicroseconds((unsigned int)tempoRestante);
     }
     proximaAmostra += periodoAmostragemUs;
 
-    double amostraMv = analogReadMilliVolts(pinoADC);
+    uint32_t amostraMv = analogReadMilliVolts(pinoADC);
 
-    // Algoritmo de Welford: calcula a variancia sem armazenar as 400 amostras.
-    // A media representa o offset DC e e removida do resultado RMS.
-    double delta = amostraMv - mediaMv;
-    mediaMv += delta / (i + 1);
-    double delta2 = amostraMv - mediaMv;
-    somaDesviosQuadrados += delta * delta2;
+    if (i == 0 || amostraMv < minimoMv) {
+      minimoMv = amostraMv;
+    }
+
+    if (i == 0 || amostraMv > maximoMv) {
+      maximoMv = amostraMv;
+    }
   }
 
-  return sqrt(somaDesviosQuadrados / numeroAmostrasRms) / 1000.0;
+  vmin = minimoMv / 1000.0f;
+  vmax = maximoMv / 1000.0f;
 }
 
 void controlarRele(float potencia) {
@@ -176,7 +175,7 @@ void controlarRele(float potencia) {
     esperaInicialConcluida = true;
   }
 
-  // Ap�s um desligamento, mantém o relé desativado por pelo menos 30 segundos.
+  // Apos um desligamento, mantem o rele desativado por pelo menos 30 segundos.
   if (releFoiDesligado &&
       agora - momentoDesligamentoRele < tempoMinimoDesligado) {
     inicioCondicaoLigar = 0;
@@ -206,17 +205,34 @@ void controlarRele(float potencia) {
   Serial.println("Relé acionado.");
 }
 
-void displayLED(float tensao, float corrente, float potencia) {
+void displayLED(float vpp, float vrms, float vmin, float vmax,
+                float corrente, float potencia) {
   display.clearDisplay();
-  display.setTextSize(2); 
   display.setTextColor(SSD1306_WHITE);
-  display.setCursor(10, 0);
-  display.print(tensao);
-  display.println(" V");
+
+  display.setTextSize(1);
+  display.setCursor(0, 0);
+  display.print("Vpp ");
+  display.print(vpp, 2);
+  display.print(" Vrms ");
+  display.print(vrms, 2);
+
+  display.setCursor(0, 9);
+  display.print("Min ");
+  display.print(vmin, 2);
+  display.print(" Max ");
+  display.print(vmax, 2);
+
+  display.setTextSize(2);
+  display.setCursor(0, 21);
+  display.print("I ");
   display.print(corrente);
-  display.println(" A");
-  display.print(potencia);
-  display.println(" W");
-  display.display();      
-  delay(100);
+  display.print(" A");
+
+  display.setCursor(0, 43);
+  display.print("P ");
+  display.print(potencia, 0);
+  display.print(" W");
+
+  display.display();
 }
